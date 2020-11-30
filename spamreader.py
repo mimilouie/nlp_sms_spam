3d3d68fd5959f71795201a8a5fe780f5d7db3118
auhnix <hava.xinhua@gmail.com>
2020-11-30 02:39:54 -0500

    lol

--- spamreader.py
+++ spamreader.py
@@ -6,6 +6,30 @@
 from scipy import sparse
 from collections import Counter
 import argparse
+import sys
+from sklearn.naive_bayes import MultinomialNB
+import numpy as np
+from sklearn.model_selection import cross_val_predict
+
+def readLabels(fp):
+    lines = fp.readlines()
+    labels = []
+    for l in lines:
+        if l[:3] == "ham":
+            labels.append("ham")
+        elif l[:4] == "spam":
+            labels.append("spam")
+    return labels
+
+def readMessages(fp):
+    lines = fp.readlines()
+    messages = []
+    for l in lines:
+        if l[:3] == "ham":
+            messages.append(l[4:-1])
+        elif l[:4] == "spam":
+            messages.append(l[5:-1])
+    return messages
 
 class BagOfWords():
     def __init__(self):
@@ -13,81 +37,102 @@ class BagOfWords():
         self.vocab = [] # add when we filter frequent/infreq words out of features
         self.labels = []
 
-    def readMessages(self, fp):
-        lines = fp.readlines()
-        messages = []
-        for l in lines:
-            if l[:3] == "ham":
-                messages.append(l[4:-1])
-            elif l[:4] == "spam":
-                messages.append(l[5:-1])
-        return messages
-
-    def getBigrams(self, msgs):
+    def getBigrams(self, msgs, lower=True):
         bigrams = []
         for msg in msgs:
             words = msg.split()
             for i in range(len(words) - 1):
                 bigram = words[i] + "-" + words[i+1]
-                bigrams.append(bigram)
-        return bigrams
+                #bigrams.append(bigram.lower()) if lower else bigrams.append(bigram)
+                if lower:
+                    bigrams.append(bigram.lower())
+                else:
+                    bigrams.append(bigram)
+        return Counter(bigrams)
 
-    def getTrigrams(self, msgs):
+    def getTrigrams(self, msgs, lower=True):
         trigrams = []
         for msg in msgs:
             words = msg.split()
             for i in range(len(words) - 2):
                 trigram = words[i] + "-" + words[i+1] + "-" + words[i+2]
-                trigrams.append(trigram)
-        return trigrams
+                #trigrams.append(trigram.lower()) if lower else trigrams.append(trigram)
+                if lower:
+                    trigrams.append(trigram.lower())
+                else:
+                    trigrams.append(trigram)
+        return Counter(trigrams)
 
-    # add vocab
-    # ngrams, different tokenization, split after punctuation (. but they might be tricky bc of links and stuff idk ! ?)
-    # capitalization
-    def makeFeatures(self, msgs):
+    def makeVocab(self, msgs, lower=True, start=None, end=None):
         words = []
         for msg in msgs:
             msgWords = msg.split() #(r'[.!]+|[\w]+')
             # different tokenization approaches l8r
             for word in msgWords: 
-                words.append(word)
-                # lowercased for now
-        cnt = Counter(words) + Counter(self.getBigrams(msgs))
-        print(cnt)
-        # keeping counter for now; we can filter by
-        # feature frequency/infrequency later
-        self.features = list(cnt.keys())
+                # <expression1> if <condition> else <expression2>
+                #print(word)
+                #words.append(word.lower()) if lower else words.append(word)
+                if lower:
+                    words.append(word.lower())
+                else:
+                    words.append(word)
+        #print(words)
+        wordCounter = Counter(words)
+        sortedWords = wordCounter.most_common()
+        wordList = [t[0] for t in sortedWords]
+        #self.vocab = wordList[start:end]
+        self.vocab = wordList
+        #print(wordList[start:end])
+    
+    # add vocab
+    # ngrams, different tokenization, split after punctuation (. but they might be tricky bc of links and stuff idk ! ?)
+    # capitalization
+    def makeFeatures(self, msgs, lower=True, start=None, end=None, bigrams=False, trigrams=False):
+        self.makeVocab(msgs, lower, start, end)
+        features = self.vocab
+        print("cnt before: \n", features)
+        if bigrams:
+            features += list(self.getBigrams(msgs, lower).keys())
+        if trigrams:
+            features += list(self.getTrigrams(msgs, lower).keys())
+        print("cnt after: \n", features)
+        self.features = features
 
-    def extractFeatures(self, features, msg):
-        counts = Counter(msg.split())
-        msgVector = [0]*len(features)
-        for i in range(len(features)):
-            if features[i] in counts:
-                msgVector[i] = counts[features[i]]
+    def extractFeatures(self, lower, msg, bigrams=False, trigrams=False):
+        cnt = Counter(msg.split()) #change to some tokenization strat later idk
+        if bigrams:
+            cnt += self.getBigrams([msg], lower)
+        if trigrams:
+            cnt += self.getTrigrams([msg], lower)
+        msgVector = [0]*len(self.features)
+        for i in range(len(self.features)):
+            if self.features[i] in cnt:
+                msgVector[i] = cnt[self.features[i]]
         return msgVector
 
     #   features -> word1, word2, ...
     #   msg1
     #   msg2
     #   ...
-    def process(self, features, msgs):
+    def process(self, msgs, lower=True, bigrams=False, trigrams=False):
         N = len(msgs)
-        M = len(features)
+        M = len(self.features)
         X = sparse.lil_matrix((N, M), dtype='uint8')
         for i in range(N): #range(N):
             if i % 20 == 0:
                 print("message #", i)
-            msgVector = self.extractFeatures(features, msgs[i])
+            msgVector = self.extractFeatures(lower, msgs[i], bigrams, trigrams)
             for j in range(M):
                 X[i,j] = msgVector[j]
         return X
 
 def main(args):
     bagOfWords = BagOfWords()
-    msgs = bagOfWords.readMessages(args.data)
+    msgs = readMessages(args.data)
     bagOfWords.readLabels(args.data)
     bagOfWords.makeFeatures(msgs)
-    X = bagOfWords.process(bagOfWords.features, msgs)
+    # dont forget parameters
+    X = bagOfWords.process(msgs)
     print(X.toarray())
 
 if __name__ == '__main__':
--- spamreader_main.py
+++ spamreader_main.py
@@ -15,30 +15,51 @@ def readLabels(fp):
             labels.append("spam")
     return labels
 
-def doExperiment(args):
-    # use vocab when we filter the features blah blah blah
-    labels = readLabels(args.training)
+# def main(args):
+#     # use vocab when we filter the features blah blah blah
+#     bagOfWords = sr.BagOfWords()
+#     f = open("150.txt",'r')
+#     print(f)
+#     g = open("output-lowered-bigrams-2.txt",'w')
+#     labels = readLabels(f)
+#     msgs = sr.readMessages(f)
+#     print("msgs [main]: \n", msgs)
+#     print("fread\n", f.read())
+#     #print("argsread\n", args.training.read())
+#     bagOfWords.makeFeatures(msgs, args.lower, args.start, args.end, args.bigrams, args.trigrams)
+#     clf = MultinomialNB()
+#     X = bagOfWords.process(msgs, args.lower, args.bigrams, args.trigrams)
+    
+#     # crossval for now
+#     probabilities_validate = cross_val_predict(clf,X,y=labels,method="predict_proba",cv=args.xvalidate)
+#     predict_validate = cross_val_predict(clf,X,y=labels,method="predict",cv=args.xvalidate)
+#     for i in range(len(msgs)):
+#         p = predict_validate[i]
+#         g.write(str(i) + " " + p + " " + str(probabilities_validate[i]) + "\n")
+
+def main(args):
     bagOfWords = sr.BagOfWords()
-    f = open("150.txt",'r')
-    g = open("output-notlowered-bigrams.txt",'w')
-    msgs = bagOfWords.readMessages(f)
-    bagOfWords.makeFeatures(msgs)
-    clf = MultinomialNB()
-    X = bagOfWords.process(bagOfWords.features, msgs)
-    # crossval for now
-    probabilities_validate = cross_val_predict(clf,X,y=labels,method="predict_proba",cv=args.xvalidate)
-    predict_validate = cross_val_predict(clf,X,y=labels,method="predict",cv=args.xvalidate)
-    for i in range(len(msgs)):
-        p = predict_validate[i]
-        g.write(str(i) + " " + p + " " + str(probabilities_validate[i]) + "\n")
+    msgs = sr.readMessages(args.training)
+    readLabels(args.training)
+    bagOfWords.makeFeatures(msgs, args.lower, args.end, args.bigrams, args.trigrams)
+    # dont forget parameters
+    X = bagOfWords.process(msgs, args.lower, args.bigrams, args.trigrams)
+    print(X.toarray())
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--training", type=argparse.FileType('r'), help="training dataset filepath")
+    parser.add_argument("training", type=argparse.FileType('r'), help="training dataset filepath")
     parser.add_argument("-o", "--output", type=argparse.FileType('w'), default=sys.stdout, help="prediction writing location")
     parser.add_argument("-x", "--xvalidate", type=int, default=10, help="crossvalidation folds")
+    parser.add_argument("-l", "--lower", action="store_true", default=False, help="use lowercase")
+    parser.add_argument("-b", "--bigrams", action="store_true", default=False, help="use bigrams")
+    parser.add_argument("-t", "--trigrams", action="store_true", default=False, help="use trigrams")
+    parser.add_argument("-s", "--start", type=int, default=None, help="begin of vocab cutoff")
+    parser.add_argument("-e", "--end", type=int, default=None, help="end of vocab cutoff")
 
     args = parser.parse_args()
-    doExperiment(args)
+    print(args)
+    main(args)
 
-    args.training.close()
+    # args.training.close()
+    # args.output.close()
